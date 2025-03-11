5c25a4cc8582ba7727ded6a8c76a4974e73d576c
Jean Gomis <jeangomistech2018@gmail.com>
2025-03-11 19:00:41 +0100

    My first commit

---
+++ src/main/java/com/jcgomis/kata_foo_bar_quix/KataFooBarQuixApplication.java
@@ -0,0 +1,13 @@
+package com.jcgomis.kata_foo_bar_quix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataFooBarQuixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataFooBarQuixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcgomis/kata_foo_bar_quix/KataFooBarQuixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcgomis.kata_foo_bar_quix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataFooBarQuixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
